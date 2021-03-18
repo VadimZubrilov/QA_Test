51f7122db9cf02e5f2806830ddabbb3c2ee974d7
Gvaihir <gvaihir588@gmail.com>
2021-03-18 08:44:22 +0300

    graddle

--- src/test/java/Tests/TextBoxTests.java
+++ src/test/java/Tests/TextBoxTests.java
@@ -1,4 +1,14 @@
 package Tests;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class TextBoxTests {
+    @Test
+    void successfulFillTest() {
+        open("https://demoqa.com/automation-practice-form");
+
+    }
 }
+
