dfc60a53b0edba317400008c815c0070992b1de9
Gvaihir <gvaihir588@gmail.com>
2021-03-20 12:48:16 +0300

    4

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -1,15 +1,32 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class Test2 {
+
+    @BeforeAll
+    // делаем расширения браузера на весь экран
+    static void setup() {
+
+        Configuration.startMaximized = true;
+    }
+
     @Test
     void successfulFillTest() {
+        // открываем сайт
         open("https://demoqa.com/automation-practice-form");
-        System.out.println("debug");
+        //Заполняем поля
+        $("#firstName").setValue("Vadim");
+        $("#lastName").setValue("Zubrilov");
+        $("#userEmail").setValue("test@mail.com");
+        $("#submit").click();
+        sleep(5000);
+
+        //$("#firstName").shouldHave(text("Vadim"));
     }
 }
