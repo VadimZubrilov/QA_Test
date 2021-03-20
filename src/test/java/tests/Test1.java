e78ef5984b5681ca1f54f906eb764885de0ee5f9
Gvaihir <gvaihir588@gmail.com>
2021-03-20 20:24:40 +0300

    8

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -6,24 +6,23 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
-
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-public class Test2 {
+public class Test1 {
 
-String Name = "Vadim";
-String LasName = "Zubrilov";
-String mail = "test@mail.com";
-String mobile ="5674567456";
-String subjects ="Computer Science";
-String Picture ="testjpg.jpg";
-String Address = "Penza58";
-String state = "Uttar Pradesh";
-String city = "Agra";
+    String Name = "Vadim";
+    String LasName = "Zubrilov";
+    String mail = "test@mail.com";
+    String mobile = "5674567456";
+    String subjects = "Computer Science";
+    String Picture = "testjpg.jpg";
+    String Address = "Penza58";
+    String state = "Uttar Pradesh";
+    String city = "Agra";
+
     @BeforeAll
     // делаем расширения браузера на весь экран
     static void setup() {
@@ -39,18 +38,15 @@ String city = "Agra";
         $("#firstName").setValue(Name);
         $("#lastName").setValue(LasName);
         $("#userEmail").setValue(mail);
-        // Выбор гендера, обычный .click() не сработал, нужно понять почему сработал .doubleClick();
         $("#gender-radio-1").doubleClick();
         $("#userNumber").setValue(mobile);
         // Выбор даты рождения
-        // selectOptionByValue узнать что это
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("9");
         $(".react-datepicker__year-select").selectOptionByValue("1991");
         $(".react-datepicker__day.react-datepicker__day--018").click();
         $("#subjectsInput").setValue(subjects).pressEnter();
         $(byText("Sports")).click();
-        //$("#uploadPicture").doubleClick(), понять как работает uploadFromClasspath
         $("#uploadPicture").uploadFromClasspath(Picture);
         $("#currentAddress").setValue(Address);
         $("#state").click();
@@ -62,7 +58,6 @@ String city = "Agra";
         $("#submit").scrollIntoView(true);
         $("#submit").click();
         sleep(2000);
-        //$("#submit").should(visible, Duration.ofSeconds(5)).click(); совет из чата (не сработал)
 
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
