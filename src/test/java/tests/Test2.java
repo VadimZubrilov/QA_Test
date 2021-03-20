f96495b00f5e9b9b49555fb2735dc673b97ba3f7
Gvaihir <gvaihir588@gmail.com>
2021-03-20 14:33:07 +0300

    5

--- src/test/java/tests/Test2.java
+++ src/test/java/tests/Test2.java
@@ -1,13 +1,20 @@
 package tests;
 
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class Test2 {
+
+
 
     @BeforeAll
     // делаем расширения браузера на весь экран
@@ -24,9 +31,15 @@ public class Test2 {
         $("#firstName").setValue("Vadim");
         $("#lastName").setValue("Zubrilov");
         $("#userEmail").setValue("test@mail.com");
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue("5674567456");
+        //sleep(5000);
+        $("#submit").scrollIntoView(true);
         $("#submit").click();
         sleep(5000);
+        //$("#submit").should(visible, Duration.ofSeconds(5)).click(); совет из чата (не сработал)
 
-        //$("#firstName").shouldHave(text("Vadim"));
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
     }
 }
